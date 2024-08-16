f78a4a51b2f33004acf8a0161403c4fc6f4b1357
kevintsai <kevintsai@TD-ID10-01460.mshome.net>
2024-08-17 01:06:43 +0800

    update

--- src/main/java/com/example/ai/controller/AiClientController.java
+++ src/main/java/com/example/ai/controller/AiClientController.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
@@ -12,17 +11,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.ai.service.ChatService;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class AiClientController {
-	private final ChatService chatService;
+	private final ChatClient chatClient;
+	private List<Message> memMessage = new ArrayList<>();
 	
 	@GetMapping("/memchat")
-	public String chat(@RequestParam String chatId, @RequestParam String prompt) {
-		return chatService.chat(chatId, prompt);
+	public String chat(@RequestParam String prompt) {
+		memMessage.add(new UserMessage(prompt));
+		ChatResponse chatResponse = chatClient.prompt()
+				.messages(memMessage)
+				.call()
+				.chatResponse();
+		memMessage.add(chatResponse.getResult().getOutput());
+		return chatResponse.getResult().getOutput().getContent();
 	}
 }
